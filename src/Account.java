ddc4de905f6cf793e59d0aeda2d7fc55c90a3434
Leonardo <leorlucena@gmail.com>
2025-04-06 12:03:45 -0300

    methods and functions

--- src/Account.java
+++ src/Account.java
@@ -2,4 +2,13 @@ public class Account {
     String accountNumber;
     String accountOwner;
     double balance;
+
+    void deposit(double amount){
+        balance += amount;
+    }
+
+    void printBalance() {
+        System.out.println("Balance: R$" + balance);
+    }
+
 }
---
+++ src/App2.java
@@ -0,0 +1,10 @@
+public class App2 {
+
+    public static void main(String[] args) {
+
+        Account a = new Account();
+        a.deposit(100);
+        a.deposit(200);
+        a.printBalance();
+    }
+}
